6056843f1561f50071c60e58e6213a503513dfa5
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-03-28 16:10:28 +0200

    transitions init

---
+++ part-7-animations-transitions/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div style="margin-top: 200px">
+    <button class="btn btn-primary" @click="show = !show">Toggle</button>
+
+    <transition>
+      <p v-if="show">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sed
+        sunt minus temporibus mollitia, vitae tenetur amet culpa odit ratione
+        voluptatum, et veritatis, excepturi quos? Porro velit neque hic optio!
+      </p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style>
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 1s;
+}
+</style>
