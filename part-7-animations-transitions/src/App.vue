4f59ef4baf1e303bf9a1b72d178aa3120180cdb5
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-03-29 18:54:57 +0200

    transition commit

--- part-7-animations-transitions/src/App.vue
+++ part-7-animations-transitions/src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div style="margin-top: 200px">
-    <button class="btn btn-primary" @click="show = !show">Toggle</button>
+  <div style="margin-top: 200px; margin-left: 200px; flex">
+    <button class="btn btn-primary" @click="show1 = !show1">Toggle</button>
 
-    <transition>
-      <p v-if="show">
+    <transition name="fade">
+      <p v-if="show1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sed
+        sunt minus temporibus mollitia, vitae tenetur amet culpa odit ratione
+        voluptatum, et veritatis, excepturi quos? Porro velit neque hic optio!
+      </p>
+    </transition>
+
+    <button class="btn btn-primary" @click="show2 = !show2">Toggle</button>
+
+    <transition
+      enter-active-class="animated pulse"
+      leave-active-class="animated zoomOut"
+    >
+      <p v-if="show2" style="width: 200px">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sed
         sunt minus temporibus mollitia, vitae tenetur amet culpa odit ratione
         voluptatum, et veritatis, excepturi quos? Porro velit neque hic optio!
@@ -13,23 +26,26 @@
 </template>
 
 <script>
+// import "animate.css";
+
 export default {
   data() {
     return {
-      show: false,
+      show1: false,
+      show2: false,
     };
   },
 };
 </script>
 
 <style>
-.v-enter,
-.v-leave-to {
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 }
 
-.v-enter-active,
-.v-leave-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: opacity 1s;
 }
 </style>
