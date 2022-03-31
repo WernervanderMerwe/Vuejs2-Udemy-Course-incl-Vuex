e244ffc519c31977cc2c4cb5815abf28af8fd758
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-03-31 17:30:30 +0200

    animate commit

--- part-7-animations-transitions/src/App.vue
+++ part-7-animations-transitions/src/App.vue
@@ -13,10 +13,20 @@
     <button class="btn btn-primary" @click="show2 = !show2">Toggle</button>
 
     <transition
-      enter-active-class="animated pulse"
-      leave-active-class="animated zoomOut"
+      enter-active-class="animate__animated animate__pulse slow"
+      leave-active-class="animate__animated animate__zoomOut"
     >
       <p v-if="show2" style="width: 200px">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sed
+        sunt minus temporibus mollitia, vitae tenetur amet culpa odit ratione
+        voluptatum, et veritatis, excepturi quos? Porro velit neque hic optio!
+      </p>
+    </transition>
+
+    <button class="btn btn-primary" @click="show3 = !show3">Toggle</button>
+
+    <transition>
+      <p v-if="show3" style="width: 200px">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sed
         sunt minus temporibus mollitia, vitae tenetur amet culpa odit ratione
         voluptatum, et veritatis, excepturi quos? Porro velit neque hic optio!
@@ -26,19 +36,52 @@
 </template>
 
 <script>
-// import "animate.css";
-
 export default {
   data() {
     return {
       show1: false,
       show2: false,
+      show3: false,
     };
   },
 };
 </script>
 
 <style>
+.v-enter-active {
+  animation: pulse 1s;
+}
+
+.v-leave-active {
+  animation: fade-out 1s;
+}
+
+@keyframes fade-out {
+  from {
+    opacity: 1;
+    transition: opacity;
+  }
+
+  to {
+    opacity: 0;
+  }
+}
+
+@keyframes pulse {
+  from {
+    transform: scale3d(1, 1, 1);
+  }
+  50% {
+    transform: scale3d(1.2, 1.2, 1.2);
+  }
+  to {
+    transform: scale3d(1, 1, 1);
+  }
+}
+
+.slow {
+  animation-duration: 2s;
+}
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
