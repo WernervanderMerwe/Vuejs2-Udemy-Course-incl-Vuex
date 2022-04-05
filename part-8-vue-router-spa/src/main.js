e9824673005e8e9a9978381e4ad7ea895a19107b
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-04-05 16:35:37 +0200

    router

--- part-8-vue-router-spa/src/main.js
+++ part-8-vue-router-spa/src/main.js
@@ -1,10 +1,29 @@
 import Vue from "vue";
-import App from "./App.vue";
 import VueRouter from "vue-router";
+import App from "./App.vue";
+import routes from "./routes.js";
 
 Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: routes,
+  mode: "history",
+});
+
+Vue.filter("currency", function (value) {
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
+  });
+
+  return formatter.format(value);
+});
+
+export const eventBus = new Vue();
 
 new Vue({
   el: "#app",
   render: (h) => h(App),
+  router: router,
 });
