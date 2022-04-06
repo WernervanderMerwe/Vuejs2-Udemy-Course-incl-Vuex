79b66e5d3be958694dbbfccb0fb40b8c03154e3b
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-04-06 19:04:24 +0200

    router

--- part-8-vue-router-spa/src/routes.js
+++ part-8-vue-router-spa/src/routes.js
@@ -1,8 +1,15 @@
 import ProductList from "./ProductList.vue";
 import Cart from "./Cart.vue";
+import ViewProduct from "./ViewProduct.vue";
 
 export default [
   { path: "", component: ProductList },
+  {
+    path: "/products/:productId",
+    props: true, // linked to the commented out code in ViewProduct
+    name: "viewProduct",
+    component: ViewProduct,
+  },
   { path: "/cart", component: Cart },
   { path: "*", component: { template: "<h1>Page Not Found</h1>" } },
 ];
