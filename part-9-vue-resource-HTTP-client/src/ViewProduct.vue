af578fec63f4ee33b83b3fb790bb2c3dab210eeb
WernervanderMerwe <wernervandermerwe@rocketmail.com>
2022-04-11 18:32:56 +0200

    resource commit

--- part-9-vue-resource-HTTP-client/src/ViewProduct.vue
+++ part-9-vue-resource-HTTP-client/src/ViewProduct.vue
@@ -1,61 +1,131 @@
 <template>
-    <div>
-        <button class="btn btn-primary" @click="goBack">&laquo; Back</button>
+  <div>
+    <button class="btn btn-primary" @click="goBack">&laquo; Back</button>
 
-        <div v-if="product != null">
-            <h1>{{ product.name }}</h1>
+    <div v-if="product != null">
+      <h1>{{ product.name }}</h1>
 
-            <p><strong>ID:</strong> {{ product.id }}</p>
-            <p><strong>Price:</strong> {{ product.price | currency }}</p>
-            <p><strong>In stock:</strong> {{ product.inStock }}</p>
-            <p>{{ product.description }}</p>
+      <p><strong>ID:</strong> {{ product.id }}</p>
+      <p><strong>Price:</strong> {{ product.price | currency }}</p>
+      <p><strong>In stock:</strong> {{ product.inStock }}</p>
+      <p>{{ product.description }}</p>
 
-            <h2>Reviews</h2>
+      <h2>Reviews</h2>
 
-            <div v-if="product.reviews.length > 0">
-                <div v-for="review in product.reviews">
-                    <strong>{{ review.reviewer }}</strong> (rating: {{ review.rating }})
-                    <p>{{ review.text }}</p>
-                </div>
-            </div>
+      <div v-if="product.reviews.length > 0">
+        <div v-for="review in product.reviews">
+          <strong>{{ review.reviewer }}</strong> (rating: {{ review.rating }})
+          <p>{{ review.text }}</p>
+        </div>
+      </div>
 
-            <div v-else>
-                <p>No reviews have been added for this product.</p>
-            </div>
+      <div v-else>
+        <p>No reviews have been added for this product.</p>
+      </div>
+
+      <h3>Add Review</h3>
+
+      <form @submit.prevent="addNewReview(newReview)">
+        <div class="form-group">
+          <label for="reviewName">Name</label>
+          <input
+            type="text"
+            v-model="newReview.reviewer"
+            class="form-control"
+            id="reviewName"
+            placeholder="Name"
+          />
         </div>
 
-        <div v-else>
-            <p>Loading...</p>
+        <div class="form-group">
+          <label for="reviewRating">Rating</label>
+          <input
+            type="number"
+            v-model.number="newReview.rating"
+            class="form-control"
+            id="reviewRating"
+            placeholder="Rating"
+          />
         </div>
+
+        <div class="form-group">
+          <label for="reviewText">Text</label>
+          <textarea
+            v-model="newReview.text"
+            class="form-control"
+            id="reviewText"
+            cols="30"
+            rows="10"
+          ></textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit Review</button>
+      </form>
     </div>
+
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: {
-            productId: {
-                required: true
-            }
-        },
-        data() {
-            return {
-                product: null
-            };
-        },
-        created() {
-            this.product = this.getProduct(this.productId);
-        },
-        beforeRouteUpdate(to, from, next) {
-            this.product = this.getProduct(to.params.productId);
-            next();
-        },
-        methods: {
-            getProduct(productId) {
+export default {
+  props: {
+    productId: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      product: null,
+      newReview: {
+        text: "",
+        rating: 0,
+        reviewer: "",
+      },
+    };
+  },
+  created() {
+    this.getProduct(this.productId).then((product) => (this.product = product));
+  },
+  beforeRouteUpdate(to, from, next) {
+    this.getProduct(to.params.productId).then(
+      (product) => (this.product = product)
+    );
 
-            },
-            goBack() {
-                this.$router.history.go(-1);
-            }
-        }
-    }
+    next();
+  },
+  methods: {
+    getProduct(productId) {
+      return this.$http
+        .get("http://localhost:3000/products/{productId}", {
+          params: {
+            productId: productId,
+          },
+        })
+        .then(
+          (response) => response.json(),
+          (response) => alert("Could not retrieve product")
+        )
+        .then((product) => (this.product = product));
+    },
+    goBack() {
+      this.$router.history.go(-1);
+    },
+    addNewReview(review) {
+      this.$http
+        .post("http://localhost:3000/products/{productId}/reviews", review, {
+          params: {
+            productId: this.product.id,
+          },
+        })
+        .then(
+          (response) => response.json(),
+          (response) => alert("Could not add review!")
+        )
+        .then((newReview) => this.product.reviews.push(newReview));
+    },
+  },
+};
 </script>
--- part-9-vue-resource-HTTP-client/src/main.js
+++ part-9-vue-resource-HTTP-client/src/main.js
@@ -1,18 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import VueRouter from 'vue-router';
-import { routes } from './routes';
-import 'bootstrap/dist/css/bootstrap.css';
+import Vue from "vue";
+import App from "./App";
+import VueRouter from "vue-router";
+import { routes } from "./routes";
+import "bootstrap/dist/css/bootstrap.css";
+import VueResource from "vue-resource";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.filter('currency', function(value) {
-  let formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 0
+Vue.filter("currency", function (value) {
+  let formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 0,
   });
 
   return formatter.format(value);
@@ -24,11 +25,11 @@ export const authService = { isLoggedIn: false };
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes: routes,
-  mode: 'history',
+  mode: "history",
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       return {
-        selector: to.hash
+        selector: to.hash,
       };
     }
 
@@ -37,11 +38,11 @@ const router = new VueRouter({
     }
 
     return { x: 0, y: 0 };
-  }
+  },
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.isAuthRequired)) {
+  if (to.matched.some((record) => record.meta.isAuthRequired)) {
     if (!authService.isLoggedIn) {
       alert("You must be logged in!");
       return next(false);
@@ -51,9 +52,11 @@ router.beforeEach((to, from, next) => {
   next();
 });
 
+Vue.use(VueResource);
+
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App),
-  router: router
-})
+  el: "#app",
+  render: (h) => h(App),
+  router: router,
+});
